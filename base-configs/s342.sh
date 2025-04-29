34b2dcdc9d429012f2cd9745d3f58db1621b0d3e
jorabada <jorge.rabadan@nokia.com>
2025-04-29 10:08:49 -0700

    modify linux configs of the servers

--- base-configs/s342.sh
+++ base-configs/s342.sh
@@ -2,8 +2,5 @@ ip addr add 10.0.34.2/24 dev eth1
 ip link add lo1 type dummy
 ip addr add 99.99.99.1/24 dev lo1
 ip link set lo1 up
-ip link add clab-vrf type vrf table 1
-ip link set dev clab-vrf up
-ip link set dev eth1 master clab-vrf
-ip link set dev lo1 master clab-vrf
-ip route add 0.0.0.0/0 via 10.0.34.254 table 1
+ip route del default
+ip route add 0.0.0.0/0 via 10.0.34.254
--- base-configs/s71.sh
+++ base-configs/s71.sh
@@ -1,5 +1,3 @@
 ip addr add 10.0.71.1/24 dev eth1
-ip link add clab-vrf type vrf table 1
-ip link set dev clab-vrf up
-ip link set dev eth1 master clab-vrf
-ip route add 0.0.0.0/0 via 10.0.71.254 table 1
+ip route del default
+ip route add 0.0.0.0/0 via 10.0.71.254
